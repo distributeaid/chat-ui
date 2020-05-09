c710fcb5bedd1b47e54a42d61a22ee361984990b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-05-09 14:47:33 +0200

    build: include env in netlify web

--- scripts/renderIndex.js
+++ scripts/renderIndex.js
@@ -1,12 +1,11 @@
 const { html, getVersion } = require('./html')
-const fs = require('fs')
-const path = require('path')
-const Handlebars = require('handlebars')
 
-console.log(
+process.stdout.write(
 	html({
 		VERSION: getVersion(),
 		IS_PRODUCTION: JSON.stringify(true),
 		SITE_DIR: process.env.SITE_DIR,
+		GRAPHQL_API_ENDPOINT: process.env.GRAPHQL_API_ENDPOINT,
+		GRAPHQL_API_KEY: process.env.GRAPHQL_API_KEY,
 	}),
 )
